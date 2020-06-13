3a8704cd1d5dccf9868b5dfe5dedcaecc3de2d8b
CheJinsen <983877388@qq.com>
2020-06-13 17:45:41 +0000

    update Normal

--- include/normal.h
+++ include/normal.h
@@ -35,15 +35,36 @@ class Normal
 public:
 	Normal() = default;
 	
+	/*
+	 * This function compute the probability density p(x) at x for
+	 * a Gaussian distribution with standard deviation mean = 0.0
+	 * and sigma = 1.0 default.
+	 */
 	static double dnorm(const double x,
 		const double mean = 0.0, const double sigma = 1.0);
-
+	
+	/*
+	 * This function compute the cumulative distribution functions.
+	 * mean = 0.0 and sigma = 1.0 default.
+	 */
 	static double pnorm(const double q,
 		const double mean = 0.0, const double sigma = 1.0);
-
+	
+	/*
+	 * This function compute the inverses cumulative distribution
+	 * function p(x), mean = 0.0, sigma = 1.0 default.
+	 */
 	static double qnorm(const double p,
 		const double mean = 0.0, const double sigma = 1.0);
-
+	
+	/*
+	 * This function returns a Gaussian random variate,
+	 * with mean zero and standard deviation sigma.
+	 * param n: number of observations.
+	 * param mean: means of random number.
+	 * param sigma: sigma of random number.
+	 * return a vector of double random number.
+	 */
 	static std::vector<double> rnorm(const double n,
 		const double mean = 0.0, const double sigma = 1.0);
 
