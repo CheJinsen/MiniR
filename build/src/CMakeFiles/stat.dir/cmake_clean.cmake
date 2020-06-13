23d8526e8bebd0bfaeb81968179122387de6d826
CheJinsen <983877388@qq.com>
2020-06-13 13:43:04 +0000

    add the frame

---
+++ build/src/CMakeFiles/stat.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../bin/stat"
+  "../../bin/stat.pdb"
+  "CMakeFiles/stat.dir/chapter8.cpp.o"
+  "CMakeFiles/stat.dir/gaussian_integral.cpp.o"
+  "CMakeFiles/stat.dir/main.cpp.o"
+  "CMakeFiles/stat.dir/normal.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
